3a23b1fdee91b82778b20af1c76df60717b11af2
kazim110 <kazim.mohammadi110@gmail.com>
2023-11-08 14:13:41 +0430

    Add after_save method and chenge order palce in post model

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,9 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  def update_liks_counter
+  after_save :update_likes_counter
+
+  def update_likes_counter
     post.update(likes_counter: post.likes.count)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-
+  
   def recent_posts
-    posts.limit(3).order(created_at: :desc)
+    posts.order(created_at: :desc).limit(3)
   end
 end
