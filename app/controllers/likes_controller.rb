8316b9d9bc8e550f21772581a7404e32ff6eafdb
Mohammad Kazim <kazim.mohammadi110@gmail.com>
2023-11-19 02:34:28 +0430

    Update likes_controller.rb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,6 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(user: current_user)
-    
 
     if @like.save
       redirect_to user_post_path(@post.author, @post), notice: 'Like added successfully.'
