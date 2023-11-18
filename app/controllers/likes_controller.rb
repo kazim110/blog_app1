cefa42fe2170232574ee463837e52533f9d9c6ca
Kazim110 <kazim.mohammadi110@gmail.com>
2023-11-19 02:12:32 +0430

    Add like option

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,7 @@ class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @like = @post.likes.new(user: current_user)
+    
 
     if @like.save
       redirect_to user_post_path(@post.author, @post), notice: 'Like added successfully.'
